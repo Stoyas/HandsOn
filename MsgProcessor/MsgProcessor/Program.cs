887a263e4f80dcda7dc8bdd4b97d444e02a143e1
Kyle Zhao <kyle.zhao2011@gmail.com>
2019-09-03 23:06:01 -0400

    initialize msgprocessor

---
+++ MsgProcessor/MsgProcessor/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MsgProcessor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
